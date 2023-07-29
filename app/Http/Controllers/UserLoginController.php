c843df73d455bfe2a8b16a77d653c9d0e56cd638
nursmdev <nur.syahrial@suitmedia.com>
2023-07-29 15:28:02 +0700

    UI Add Certification

--- app/Http/Controllers/UserLoginController.php
+++ app/Http/Controllers/UserLoginController.php
@@ -42,6 +42,52 @@ class UserLoginController extends Controller
             "current_status" => $current_status,
         ]);
     }
+    public function addSertif()
+    {
+        $list_file = collect([
+            [
+                "name" => "surat-permohonan",
+                "title" => 'Surat Permohonan'
+            ],
+            [
+                "name" => "formulir-pendaftaran",
+                "title" => 'Formulir Pendaftaran'
+            ],
+            [
+                "name" => "aspek-legal",
+                "title" => 'Aspek Legal'
+            ],
+            [
+                "name" => "penyelia-halal",
+                "title" => 'Dokumen Penyelia Halal '
+            ],
+            [
+                "name" => "daftar-produk",
+                "title" => 'Daftar Nama Produk dan Bahan/Menu/Barang'
+            ],
+            [
+                "name" => "proses-pengolahan",
+                "title" => 'Proses Pengolahan Produk'
+            ],
+            [
+                "name" => "jaminan-halal",
+                "title" => 'Sistem Jaminan Halal'
+            ],
+            [
+                "name" => "salinan-sertif",
+                "title" => 'Salinan Sertifikat Halal (Bagi Pembaruan)'
+            ],
+            [
+                "name" => "lainnya",
+                "title" => 'Lainnya'
+            ],
+        ]);
+        return view('user-login.add-sertifikasi', [
+            "title" => "Tambah Sertifikasi",
+            "active" => 'sertifikasi',
+            "list_file" => $list_file
+        ]);
+    }
     public function kelus(){
         return view('user-login.kelus',[
             "title" => "kelus",
