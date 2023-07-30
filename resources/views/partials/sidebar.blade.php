19c8f1e8dec5eb41c54730f9420ba05da75214c6
nursmdev <nur.syahrial@suitmedia.com>
2023-07-30 08:10:19 +0700

    Adjust Footer

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -11,27 +11,12 @@
         </a>
     </li>
     <hr class="sidebar-divider" />
-    @can('bpjph')
-    <li class="nav-item {{ $title === '' ? 'active' : '' }}">
-        <a class="nav-link" href="/kelus">
-            <i class="fas fa-fw fa-table"></i>
-            <span>Kelola user</span>
-        </a>
-    </li>
-    <hr class="sidebar-divider" />
-    @endcan
     <li class="nav-item {{ $title === 'sertifikasi' ? 'active' : '' }}">
         <a class="nav-link {{ $title === 'sertifikasi' ? 'active' : '' }}" href="{{ url('/sertifikasi') }}/">
             <i class="fas fa-clipboard"></i>
             <span>Sertifikasi</span>
         </a>
     </li>
-    {{--<li class="nav-item {{ $title === '' ? 'active' : '' }}">
-    <a class="nav-link" href="tables.html">
-        <i class="fas fa-fw fa-table"></i>
-        <span>Bantuan</span>
-    </a>
-    </li>--}}
     <hr class="sidebar-divider d-none d-md-block" />
     <div class="text-center d-none d-md-inline">
         <button class="rounded-circle border-0" id="sidebarToggle"></button>
